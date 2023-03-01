d7a04d0ba9e2e7b6af72f27ae471025d01c80329
dadada <dadada dadada@dadada.li>
2023-03-01 11:25:27 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "master thesis data";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.devshell.url = "github:numtide/devshell";
+
+  outputs = { self, devshell, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlays.default
+          ];
+        };
+        pythonPackages = p: with p; [
+          black
+          pandas
+          seaborn
+          numpy
+          python-lsp-server
+        ];
+        python = pkgs.python3.withPackages pythonPackages;
+      in
+      {
+        formatter = nixpkgs.nixpkgs-fmt;
+        devShells.master-thesis = 
+         pkgs.devshell.mkShell {
+            name = "master-thesis-devshell";
+            packages = with pkgs; [
+              python
+            ];
+            commands = [ ];
+        };
+        devShells.default = self.devShells.${system}.master-thesis;
+      });
+}
---
+++ src/master_thesis/data.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+
+def foo():
+    print("bar")
