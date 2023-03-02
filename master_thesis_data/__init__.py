52c2523bfeb8b0251eb019073e895eded407240d
dadada <dadada@dadada.li>
2023-03-02 15:06:39 +0100

    fix decoder

--- master_thesis_data/__init__.py
+++ master_thesis_data/__init__.py
@@ -3,11 +3,14 @@ from pandas import DataFrame, Series, read_csv
 
 def decode():
   df = read_csv(argv[1])
-  gpios = df[["Channel %d" % c for c in range(1, 8)]]
-  df['decoded'] = gpios.apply(decode_bytes, axis=1)
-  df = df[["Time [s]", "decoded"]]
-  df = df.loc[df['decoded'].shift() != df['decoded']]
-  print(df)
+  df['data'] = df[channel_labels(0, 4)].apply(decode_bytes, axis=1)
+  df['event'] = df[channel_labels(4, 8)].apply(decode_bytes, axis=1)
+  df = df[["Time [s]", "event", "data"]]
+  df = df.loc[(df['event'].shift() != df['event']) & (df['data'].shift() != df['data'])]
+  print(df.head(100))
 
 def decode_bytes(c: Series) -> int:
-  return sum(x<<i for x, i in enumerate(c[::-1]))
+  return sum(x<<i for i, x in enumerate(c))
+
+def channel_labels(start, stop) -> [str]:
+  return ["Channel %d" % c for c in range(start, stop)]
