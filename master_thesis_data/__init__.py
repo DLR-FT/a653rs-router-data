9aec75543f0e1d7967f52ad5b48cbaa9b3f0511d
dadada <dadada@dadada.li>
2023-03-06 11:27:41 +0100

    decode EchoEvent

--- master_thesis_data/__init__.py
+++ master_thesis_data/__init__.py
@@ -14,6 +14,21 @@ class TraceEvent(Enum):
     ForwardToNetwork = 7
     ForwardToApex = 8
     VirtualLinkScheduled = 9
+    Echo = 10
+
+    @classmethod
+    def try_from_int(cls, val):
+        try:
+            return cls(val)
+        except:
+            None
+
+
+class EchoEvent(Enum):
+    EchoRequestSend = 0,
+    EchoRequestReceived = 1,
+    EchoReplySend = 2,
+    EchoReplyReceived = 3,
 
     @classmethod
     def try_from_int(cls, val):
@@ -27,7 +42,8 @@ def decode(df: DataFrame) -> DataFrame:
     df["data"] = df[channel_labels(0, 4)].apply(decode_bytes, axis=1)
     df["event"] = df[channel_labels(4, 8)].apply(decode_bytes, axis=1)
     df["event"] = df["event"].apply(TraceEvent.try_from_int)
-    df = df[["Time [s]", "event", "data"]]
+    df["echo"] = df[["event", "data"]].apply(decode_echo, axis=1)
+    df = df[["Time [s]", "event", "data", "echo"]]
     df = df.loc[df["event"].shift() != df["event"]]
     return df
 
@@ -48,3 +64,10 @@ def decode_file():
     else:
         output = stdout
     decode(df).to_csv(path_or_buf=output)
+
+
+def decode_echo(df: Series) -> EchoEvent|None:
+    if df[0] is TraceEvent.Echo:
+        return EchoEvent.try_from_int(df[1])
+    else:
+        return None
